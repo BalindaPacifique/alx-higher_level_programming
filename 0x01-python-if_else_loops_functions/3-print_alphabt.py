ae3ac1231ac3a3ddeb5869e73d22704301a926f2
BalindaPacifique <paccybalinda@gmail.com>
2023-11-29 15:46:22 -0800

    except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alphabet_low in range(97, 123):
-    if (alphabet_low == 101) or(alphabet_low == 113):
+    if alphabet_low == 101 or alphabet_low == 113:
         continue
     print("{}".format(chr(alphabet_low)), end='')
