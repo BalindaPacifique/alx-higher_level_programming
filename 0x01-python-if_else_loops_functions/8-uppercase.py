ed7b63497201ab2e75f10428434ef9dbc8227f4b
BalindaPacifique <paccybalinda@gmail.com>
2023-11-29 17:13:05 -0800

    uppercases

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for iter in str:
+        file = iter
+        if ord(file) >= 97 and ord(file) < 123:
+            file = chr(ord(iter) - 32)
+            print(f"{file}", end="")
