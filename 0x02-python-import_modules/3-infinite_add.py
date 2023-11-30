eaf700cb597480ef9e91fc9ef765623fabc84ec7
BalindaPacifique <paccybalinda@gmail.com>
2023-11-30 13:53:16 -0800

    infinity sum of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+def sum_arg(argv):
+    i = len(argv) - 1
+    if i == 0:
+        print("{:d}".format(i))
+        return
+    else:
+        numbers_args = 1
+        sum_args = 0
+        while numbers_args <= i:
+            sum_args += int(argv[numbers_args])
+            numbers_args = numbers_args + 1
+        print("{:d}".format(sum_args))
 
 if __name__ == "__main__":
     import sys
-
-    sum_add = 0
-    for numbers_args in range(len(sys.argv) - 1):
-        sum_add = sum_add + int(sys.argv[numbers_args + 1])
-        print("{}".format(sum_add))
+    sum_arg(sys.argv)
